b97c2f401d5fd95971070b19cc487fddd5439803
Wonde-16 <115214551+Wonde-16@users.noreply.github.com>
2023-04-06 22:45:40 +0200

    g

--- js/estilo.js
+++ js/estilo.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll",function(){
     const  sectionPrimera  =document.querySelector(".primero");
     let al1 = sectionPrimera.getBoundingClientRect();
 
-    if(al1.top <= "800" && cont == 0){
+    if(al1.top <= "100" && cont == 0){
         var countUp = new CountUp('contador1', 32000);
         countUp.start();
         var countUp = new CountUp('contador2', 7000);
